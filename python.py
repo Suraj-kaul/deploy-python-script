d4ef703d8f0879229bc0d913797ba559df01db00
suraj <surajkaul15@gmail.com>
2022-05-11 16:09:29 +0530

    input-python

--- python.py
+++ python.py
@@ -1,14 +1,16 @@
-
 import sys
-first_num = sys.argv[1]
-last_num = sys.argv[2]
-
-number = 1
 
 
-if number > 1:
-    for i in range(2, number):
+first_number = int(sys.argv[1])
+last_number = int(sys.argv[2])
+
+
+
+for number in range(first_number,last_number+1):
+
+    if number > 1:
+        for i in range(2, number):
             if(number%i==0):
                 break
-            else:
-                print("prime numbers:",number)
+        else:
+            print("prime numbers:",number)
