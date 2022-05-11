9ba9c9a4ad34926aa92af30442043eb02f9be971
suraj <surajkaul15@gmail.com>
2022-05-11 16:00:03 +0530

    input-python

--- python.py
+++ python.py
@@ -1,15 +1,14 @@
-# PRIME NUMBER WITHIN RANGE
 
-first_number = 1
-last_number = 20
+import sys
+first_num = sys.argv[1]
+last_num = sys.argv[2]
 
-print("range of prime number are :")
+number = 1
 
-for number in range(first_number,last_number+1):
 
-    if number > 1:
-        for i in range(2, number):
+if number > 1:
+    for i in range(2, number):
             if(number%i==0):
                 break
-        else:
-            print(number, end=" ")
+            else:
+                print("prime numbers:",number)
